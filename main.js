b938decf384e04395338f9fb759e926123ce6acd
eriyknas <anastasia9cheshuina@yandex.ru>
2026-03-17 15:43:02 +0300

    Step 5: loops in JavaScript

---
+++ main.js
@@ -0,0 +1,72 @@
+// for (let i = 0; i < 5; i++) {
+//     console.log("i =", i);
+// }
+
+
+// console.log("Числа от 1 до 10:");
+// for (let i = 1; i <= 10; i++) {
+//     console.log(i);
+// }
+// console.log("Чётные числа:");
+// for (let i = 1; i <= 10; i++) {
+//     if (i % 2 === 0) {
+//         console.log(i);
+//     }
+// }
+// let sum = 0;
+// for (let i = 1; i <= 10; i++) {
+//     sum += i;
+// }
+// console.log(sum);
+
+
+// let count = 0;
+// while (count < 3) {
+//     console.log("Count:", count);
+//     count++;
+// }
+
+
+// let number = 5;
+// while (number >= 0) {
+//     console.log(number);
+//     number--;
+// }
+
+
+// let doValue = 0;
+// do {
+//     console.log("Value:", doValue);
+//     doValue++;
+// } while (doValue < 3);
+
+
+// for (let i = 0; i < 10; i++) {
+//     if (i === 5) {
+//         break;
+//     }
+//     console.log(i);
+// }
+
+
+// for (let i = 1; i <= 10; i++) {
+//     if (i === 5) continue;
+//     if (i === 8) break;
+//     console.log(i);
+// }
+
+
+// for (let i = 1; i <= 3; i++) {
+//     for (let j = 1; j <= 3; j++) {
+//         console.log(`i = ${i}, j = ${j}`);
+//     }
+// }
+
+
+// for (let i = 1; i <= 4; i++) {
+//     let stars = "";
+//     if (i === 1 || i === 2) stars = "*";
+//     else if (i === 3) stars = "***";
+//     else if (i === 4) stars = "****";
+//     console.log(stars);
+// }
